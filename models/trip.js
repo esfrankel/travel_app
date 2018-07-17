9efa2347ab9ca5aa004aa37eee361aef746abe39
hyip8000 <hyip@purdue.edu>
2018-07-17 14:58:54 -0700

    fixed models/trip.js syntax error

--- models/trip.js
+++ models/trip.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const TripSchema = new Schema({
-  name: { type: String, required = true},
-  desc: { type: String, required = true},
+const TripSchema = new Schema ({
+  name: { type: String, required: true},
+  desc: { type: String, required: true},
   events: [{ type: Schema.Types.ObjectId }]
 });
 
