9a265ca40c40e8356b7f607c23b3b418908ef9cb
obiebryant <41204327+obiebryant@users.noreply.github.com>
2018-07-17 17:09:26 -0400

    Updated the trips routes (index, create, new, edit & show)

---
+++ routes/trips.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('./helpers/auth');
+const Trip = require('../models/trip');
+
+// Trips Index
+router.get('/', (req, res, next) =>{
+  res.render('/');
+})
+
+//Trips Create
+router.post('/', auth.requireLogin, (req, res, next) => {
+    let trip = new Trip(req.body);
+
+    trip.save(function(err, room) {
+      if(err) { console.error(err) };
+
+      return res.redirect('/trips');
+    });
+  });
+
+//Trip new
+router.get('/new', auth.requireLogin, (req, res, next) => {
+  res.render('trips/new');
+});
+
+//Trip edit
+router.get(':id/edit', auth.requireLogin, (req, res, next) => {
+  Trip.findById(req.params.id, (err, trip) => {
+    if (err) {console.error(err)};
+
+    res.render('trips/edit', {trip: trip});
+    })
+});
+
+//Trip show
+router.get('/:id', auth.requireLogin, (req, res, next) => {
+    Trip.findById(req.params.id, (err, room) => {
+      if(err) { console.error(err) };
+
+      res.render('trips/show', { trip: trip });
+    });
+  });
