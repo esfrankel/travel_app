23e17e9ff1d8b347c1e8f2bab83c9d63fb4d3c6b
Eric Frankel <eric-frankel@cox.net>
2018-07-17 16:07:04 -0700

    working on routes

--- routes/trips.js
+++ routes/trips.js
@@ -4,51 +4,28 @@ const router = express.Router();
 const auth = require('./helpers/auth');
 const Trip = require('../models/trip');
 
-// Trips Index
-router.get('/', (req, res, next) =>{
-  Trip.find({}, (err, trip) => {
+router.get('/', (req, res, next) => {
+  Trip.find({}, 'topic', function(err, trips) {
     if (err) {
-      console.log(err);
+      console.error(err);
     }
-
-    res.render('trips/index', {
-      trip: trip
-    });
+    res.render('trips/index', { trips: trips });
   });
 });
 
-//Trips Create
-router.post('/', auth.requireLogin, (req, res, next) => {
-    let trip = new Trip(req.body);
+router.get('/new', (req, res, next) =>{
+  res.render('trips/new')
+})
 
-    trip.save(function(err, room) {
-      if(err) { console.error(err) };
+router.post('/', (req, res, next) => {
+  let trip = new Trip(req.body);
 
-      return res.redirect('/trips');
-    });
+  trip.save(function(err, trip) {
+    if (err) {
+      console.error(err);
+    }
+    return res.redirect('/trips');
   });
-
-//Trip new
-router.get('/new', auth.requireLogin, (req, res, next) => {
-  res.render('trips/new');
 });
-
-//Trip edit
-router.get(':id/edit', auth.requireLogin, (req, res, next) => {
-  Trip.findById(req.params.id, (err, trip) => {
-    if (err) {console.error(err)};
-
-    res.render('trips/edit', {trip: trip});
-    })
-});
-
-//Trip show
-router.get('/:id', auth.requireLogin, (req, res, next) => {
-    Trip.findById(req.params.id, (err, trip) => {
-      if(err) { console.error(err) };
-
-      res.render('trips/show', { trip: trip });
-    });
-  });
 
 module.exports = router;
