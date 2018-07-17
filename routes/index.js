3926f11c1a8397c74c7ac3cd5be40a80f77d3caf
Eric Frankel <eric-frankel@cox.net>
2018-07-17 10:24:30 -0700

    added index and users routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require('../models/user.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = express.Router();
+
+// Set layout variables
+router.use((req, res, next) => [
+  res.locals.title = 'WanderList';
+  res.locals.currentUserId = req.session.userId;
+
+  next();
+]);
+
+// Home Page
+router.get('/',(req, res) => {
+  res.render('index');
+});
+
+// login
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// POST login
+router.post('/login', (req, res, next) => {
+  User.authenticate(req.body.username,
+  req.body.password, (err, user) => {
+    if (err || !user) {
+      const next_error = new Error("Username or password incorrect");
+      next_error.status = 401;
+
+      return next(next_error);
+    } else {
+      req.session.userId =  user._id;
+
+      return res.redirect('/');
+    }
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const User = require('../models/user');
 const auth = require('./helpers/auth')
-var router = express.Router();
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', auth.requireLogin, (req, res, next) => {
+  User.find({}, 'username', function(err, users) {
+    if(err) {
+      res.render('users/new');
+    }
+    res.render('trips/index');
+  });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('users/new');
+});
+
+/* create users */
+router.post('/', (req, res, next) => {
+  const user = new User(req.body);
+
+  user.save(function(err, user) {
+    if (err) {
+      console.log(err);
+    }
+    return res.redirect('trips/index');
+  });
 });
 
 module.exports = router;
