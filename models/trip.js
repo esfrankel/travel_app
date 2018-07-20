6f3f6400736777a585505e59ec7d0fc15d2353b4
hyip8000 <hyip@purdue.edu>
2018-07-20 14:49:07 -0700

    got the dates to appear below each trip FUCKKKKKKKKKKKKKKKKKKK

--- models/trip.js
+++ models/trip.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const TripSchema = new Schema ({
   name: { type: String, required: true},
   date: {type: Date, default: Date.now},
+  datefrom: {type: Date, required: true},
+  dateto: {type: Date, required: true},
   totalCost: {type: Number},
   users: [{ type: Schema.Types.ObjectId, ref: 'User'}]
 });
